a51674dec95447fd7084d7af850431219c62202e
Сергей <s.v.yakovlev@yandex.ru>
2022-09-08 21:49:04 +0400

    Initial commit

---
+++ src/main/java/yakovlev/shoping/ShopingApplication.java
@@ -0,0 +1,13 @@
+package yakovlev.shoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingApplication.class, args);
+	}
+
+}
